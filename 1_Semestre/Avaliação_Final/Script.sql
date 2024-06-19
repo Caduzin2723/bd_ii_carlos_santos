fe47b686b7276c4a1f8a756f14eb52a6bf956d9f
Carlos Santos <carlos272.santos@gmail.com>
2024-06-19 10:35:57 -0300

    Crianção da Avaliação Final

---
+++ 1_Semestre/Avaliação_Final/Script.sql
@@ -0,0 +1,7 @@
+/*SELECT * 
+FROM Login 
+WHERE Username = 'mariasilva' AND Senha = 'senha123'; Script das informações de login*/
+
+/*SELECT Nome
+FROM Funcionarios
+Where ID_filial = '1'; Script de funcionários de cada filial*/
