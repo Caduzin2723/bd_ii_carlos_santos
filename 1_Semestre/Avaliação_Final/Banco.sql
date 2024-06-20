12a4b5d27a0392443b2396f4ef8d269a612c58a1
Carlos Santos <carlos2723.santos@gmail.com>
2024-06-20 11:03:40 -0300

    Finalização da Avaliação Final

--- 1_Semestre/Avaliação_Final/Banco.sql
+++ 1_Semestre/Avaliação_Final/Banco.sql
@@ -1,3 +1,4 @@
+/*@author Carlos Santos*/
 DROP DATABASE IF EXISTS Restaurante;
 
 CREATE DATABASE Restaurante;
--- 1_Semestre/Avaliação_Final/Script.sql
+++ 1_Semestre/Avaliação_Final/Script.sql
@@ -1,15 +1,21 @@
-/*SELECT * 
+/*@author Carlos Santos*/
+
+SELECT * 
 FROM Login 
 WHERE Username = 'mariasilva' AND Senha = 'senha123'; /*Script das informações de login*/
 
-/*SELECT Nome
+SELECT *
+FROM Clientes
+WHERE ID_cliente = '1';
+
+SELECT Nome
 FROM Funcionarios
 Where ID_filial = '1'; /*Script de funcionários de cada filial*/
 
-/*SELECT Nome, ID_filial
+SELECT Nome, ID_filial
 FROM Clientes
 ORDER BY ID_filial ASC; /*Script de cada cliente organizado por filial*/
 
-/*SELECT *
+SELECT *
 FROM Pratos
 Where Valor > 19.00; /*Script onde o gerente que escolher pratos acima de R$19 para ter desconto*/
