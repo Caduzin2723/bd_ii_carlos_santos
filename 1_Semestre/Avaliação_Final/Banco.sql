d16b3f6418d90b9e419ebd9d2124eff486e084bf
Carlos Santos <carlos2723.santos@gmail.com>
2024-06-19 22:50:50 -0300

    Atualização dos Script's

--- 1_Semestre/Avaliação_Final/Banco.sql
+++ 1_Semestre/Avaliação_Final/Banco.sql
@@ -4,6 +4,17 @@ CREATE DATABASE Restaurante;
 
 USE Restaurante;
 
+-- Tabela de Filiais
+CREATE TABLE IF NOT EXISTS Filiais(
+    ID_filial int(3) AUTO_INCREMENT,
+    Endereco varchar(255),
+    Email varchar(255),
+    Telefone varchar(12),
+    Quant_mesas int(3),
+    Avaliacao decimal(4,2),
+    PRIMARY KEY (ID_filial)
+);
+
 -- Tabela de Clientes
 CREATE TABLE IF NOT EXISTS Clientes(
     ID_cliente int(8) AUTO_INCREMENT,
@@ -16,7 +27,9 @@ CREATE TABLE IF NOT EXISTS Clientes(
     Email varchar(255),
     Telefone varchar(12),
     Data_cadastro datetime,
-    PRIMARY KEY (ID_cliente)
+    ID_filial int(3),
+    PRIMARY KEY (ID_cliente),
+    FOREIGN KEY (ID_filial) REFERENCES Filiais(ID_filial)
 );
 
 -- Tabela de Login
@@ -27,17 +40,6 @@ CREATE TABLE IF NOT EXISTS Login(
     Senha varchar(255) NOT NULL,
     PRIMARY KEY (ID_login),
     FOREIGN KEY (ID_cliente) REFERENCES Clientes(ID_cliente)
-);
-
--- Tabela de Filiais
-CREATE TABLE IF NOT EXISTS Filiais(
-    ID_filial int(3) AUTO_INCREMENT,
-    Endereco varchar(255),
-    Email varchar(255),
-    Telefone varchar(12),
-    Quant_mesas int(3),
-    Avaliacao decimal(4,2),
-    PRIMARY KEY (ID_filial)
 );
 
 -- Tabela de Funcionários
@@ -149,18 +151,26 @@ CREATE TABLE IF NOT EXISTS Entregas(
     FOREIGN KEY (ID_pedido) REFERENCES Pedidos(ID_pedido)
 );
 
+-- Inserir dados na tabela Filiais
+INSERT INTO Filiais (Endereco, Email, Telefone, Quant_mesas, Avaliacao) VALUES
+('Rua K, 123', 'filial1@restaurante.com', '1111111111', 10, 4.5),
+('Rua L, 456', 'filial2@restaurante.com', '2222222222', 20, 4.0),
+('Rua M, 789', 'filial3@restaurante.com', '3333333333', 15, 4.2),
+('Rua N, 101', 'filial4@restaurante.com', '4444444444', 12, 4.7),
+('Rua O, 202', 'filial5@restaurante.com', '5555555555', 18, 4.3);
+
 -- Inserir dados na tabela Clientes
-INSERT INTO Clientes (CPF, Nome, Senha, Sexo, Idade, Endereco, Email, Telefone, Data_cadastro) VALUES 
-('12345678901', 'Maria Silva', 'senha123', 'Feminino', '25', 'Rua A, 123', 'maria.silva@gmail.com', '9876543210', '2023-01-01 10:00:00'),
-('23456789012', 'João Souza', 'senha234', 'Masculino', '30', 'Rua B, 456', 'joao.souza@gmail.com', '9876543211', '2023-01-02 11:00:00'),
-('34567890123', 'Ana Pereira', 'senha345', 'Feminino', '22', 'Rua C, 789', 'ana.pereira@gmail.com', '9876543212', '2023-01-03 12:00:00'),
-('45678901234', 'Carlos Santos', 'senha456', 'Masculino', '28', 'Rua D, 101', 'carlos.santos@gmail.com', '9876543213', '2023-01-04 13:00:00'),
-('56789012345', 'Julia Oliveira', 'senha567', 'Feminino', '26', 'Rua E, 202', 'julia.oliveira@gmail.com', '9876543214', '2023-01-05 14:00:00'),
-('67890123456', 'Pedro Lima', 'senha678', 'Masculino', '29', 'Rua F, 303', 'pedro.lima@gmail.com', '9876543215', '2023-01-06 15:00:00'),
-('78901234567', 'Mariana Costa', 'senha789', 'Feminino', '27', 'Rua G, 404', 'mariana.costa@gmail.com', '9876543216', '2023-01-07 16:00:00'),
-('89012345678', 'Lucas Almeida', 'senha890', 'Masculino', '24', 'Rua H, 505', 'lucas.almeida@gmail.com', '9876543217', '2023-01-08 17:00:00'),
-('90123456789', 'Larissa Carvalho', 'senha901', 'Feminino', '23', 'Rua I, 606', 'larissa.carvalho@gmail.com', '9876543218', '2023-01-09 18:00:00'),
-('01234567890', 'Ricardo Araújo', 'senha012', 'Masculino', '31', 'Rua J, 707', 'ricardo.araujo@gmail.com', '9876543219', '2023-01-10 19:00:00');
+INSERT INTO Clientes (CPF, Nome, Senha, Sexo, Idade, Endereco, Email, Telefone, Data_cadastro, ID_filial) VALUES 
+('12345678901', 'Maria Silva', 'senha123', 'Feminino', '25', 'Rua A, 123', 'maria.silva@gmail.com', '9876543210', '2023-01-01 10:00:00', 1),
+('23456789012', 'João Souza', 'senha234', 'Masculino', '30', 'Rua B, 456', 'joao.souza@gmail.com', '9876543211', '2023-01-02 11:00:00', 2),
+('34567890123', 'Ana Pereira', 'senha345', 'Feminino', '22', 'Rua C, 789', 'ana.pereira@gmail.com', '9876543212', '2023-01-03 12:00:00', 3),
+('45678901234', 'Carlos Santos', 'senha456', 'Masculino', '28', 'Rua D, 101', 'carlos.santos@gmail.com', '9876543213', '2023-01-04 13:00:00', 4),
+('56789012345', 'Julia Oliveira', 'senha567', 'Feminino', '26', 'Rua E, 202', 'julia.oliveira@gmail.com', '9876543214', '2023-01-05 14:00:00', 5),
+('67890123456', 'Pedro Lima', 'senha678', 'Masculino', '29', 'Rua F, 303', 'pedro.lima@gmail.com', '9876543215', '2023-01-06 15:00:00', 1),
+('78901234567', 'Mariana Costa', 'senha789', 'Feminino', '27', 'Rua G, 404', 'mariana.costa@gmail.com', '9876543216', '2023-01-07 16:00:00', 2),
+('89012345678', 'Lucas Almeida', 'senha890', 'Masculino', '24', 'Rua H, 505', 'lucas.almeida@gmail.com', '9876543217', '2023-01-08 17:00:00', 3),
+('90123456789', 'Larissa Carvalho', 'senha901', 'Feminino', '23', 'Rua I, 606', 'larissa.carvalho@gmail.com', '9876543218', '2023-01-09 18:00:00', 4),
+('01234567890', 'Ricardo Araújo', 'senha012', 'Masculino', '31', 'Rua J, 707', 'ricardo.araujo@gmail.com', '9876543219', '2023-01-10 19:00:00', 5);
 
 -- Inserir dados na tabela Login
 INSERT INTO Login (ID_cliente, Username, Senha) VALUES
@@ -175,19 +185,6 @@ INSERT INTO Login (ID_cliente, Username, Senha) VALUES
 (9, 'larissacarvalho', 'senha901'),
 (10, 'ricardoaraujo', 'senha012');
 
--- Inserir dados na tabela Filiais
-INSERT INTO Filiais (Endereco, Email, Telefone, Quant_mesas, Avaliacao) VALUES
-('Rua K, 123', 'filial1@restaurante.com', '1111111111', 10, 4.5),
-('Rua L, 456', 'filial2@restaurante.com', '2222222222', 20, 4.0),
-('Rua M, 789', 'filial3@restaurante.com', '3333333333', 15, 4.2),
-('Rua N, 101', 'filial4@restaurante.com', '4444444444', 12, 4.7),
-('Rua O, 202', 'filial5@restaurante.com', '5555555555', 18, 4.3),
-('Rua P, 303', 'filial6@restaurante.com', '6666666666', 8, 4.8),
-('Rua Q, 404', 'filial7@restaurante.com', '7777777777', 25, 4.1),
-('Rua R, 505', 'filial8@restaurante.com', '8888888888', 30, 4.6),
-('Rua S, 606', 'filial9@restaurante.com', '9999999999', 22, 4.4),
-('Rua T, 707', 'filial10@restaurante.com', '1010101010', 14, 4.9);
-
 -- Inserir dados na tabela Funcionarios
 INSERT INTO Funcionarios (CPF, Nome, Sexo, Idade, Endereco, Email, Telefone, ID_filial, Cargo, Data_contatacao) VALUES
 ('11111111111', 'José da Silva', 'Masculino', '35', 'Rua U, 808', 'jose.silva@gmail.com', '1212121212', 1, 'Garçom', '2023-02-01 10:00:00'),
@@ -195,11 +192,11 @@ INSERT INTO Funcionarios (CPF, Nome, Sexo, Idade, Endereco, Email, Telefone, ID_
 ('33333333333', 'Pedro Ferreira', 'Masculino', '30', 'Rua W, 101', 'pedro.ferreira@gmail.com', '1414141414', 3, 'Gerente', '2023-02-03 12:00:00'),
 ('44444444444', 'Mariana Costa', 'Feminino', '26', 'Rua X, 202', 'mariana.costa@gmail.com', '1515151515', 4, 'Atendente', '2023-02-04 13:00:00'),
 ('55555555555', 'Lucas Pereira', 'Masculino', '32', 'Rua Y, 303', 'lucas.pereira@gmail.com', '1616161616', 5, 'Chef', '2023-02-05 14:00:00'),
-('66666666666', 'Larissa Almeida', 'Feminino', '29', 'Rua Z, 404', 'larissa.almeida@gmail.com', '1717171717', 6, 'Auxiliar de Cozinha', '2023-02-06 15:00:00'),
-('77777777777', 'Ricardo Gomes', 'Masculino', '34', 'Rua AA, 505', 'ricardo.gomes@gmail.com', '1818181818', 7, 'Gerente', '2023-02-07 16:00:00'),
-('88888888888', 'Fernanda Santos', 'Feminino', '27', 'Rua BB, 606', 'fernanda.santos@gmail.com', '1919191919', 8, 'Cozinheira', '2023-02-08 17:00:00'),
-('99999999999', 'Carlos Oliveira', 'Masculino', '31', 'Rua CC, 707', 'carlos.oliveira@gmail.com', '2020202020', 9, 'Garçom', '2023-02-09 18:00:00'),
-('00000000000', 'Julia Araújo', 'Feminino', '33', 'Rua DD, 808', 'julia.araujo@gmail.com', '2121212121', 10, 'Atendente', '2023-02-10 19:00:00');
+('66666666666', 'Larissa Almeida', 'Feminino', '29', 'Rua Z, 404', 'larissa.almeida@gmail.com', '1717171717', 1, 'Auxiliar de Cozinha', '2023-02-06 15:00:00'),
+('77777777777', 'Ricardo Gomes', 'Masculino', '34', 'Rua AA, 505', 'ricardo.gomes@gmail.com', '1818181818', 2, 'Gerente', '2023-02-07 16:00:00'),
+('88888888888', 'Fernanda Santos', 'Feminino', '27', 'Rua BB, 606', 'fernanda.santos@gmail.com', '1919191919', 3, 'Cozinheira', '2023-02-08 17:00:00'),
+('99999999999', 'Carlos Oliveira', 'Masculino', '31', 'Rua CC, 707', 'carlos.oliveira@gmail.com', '2020202020', 4, 'Garçom', '2023-02-09 18:00:00'),
+('00000000000', 'Julia Araújo', 'Feminino', '33', 'Rua DD, 808', 'julia.araujo@gmail.com', '2121212121', 5, 'Atendente', '2023-02-10 19:00:00');
 
 -- Inserir dados na tabela Reservas
 INSERT INTO Reservas (Data_reserva, ID_cliente, Mesa, ID_filial, Capacidade) VALUES
@@ -208,11 +205,11 @@ INSERT INTO Reservas (Data_reserva, ID_cliente, Mesa, ID_filial, Capacidade) VAL
 ('2023-03-03 21:00:00', 3, 'M3', 3, '6'),
 ('2023-03-04 18:00:00', 4, 'M4', 4, '8'),
 ('2023-03-05 19:30:00', 5, 'M5', 5, '4'),
-('2023-03-06 20:30:00', 6, 'M6', 6, '2'),
-('2023-03-07 21:30:00', 7, 'M7', 7, '6'),
-('2023-03-08 18:30:00', 8, 'M8', 8, '8'),
-('2023-03-09 19:00:00', 9, 'M9', 9, '4'),
-('2023-03-10 20:00:00', 10, 'M10', 10, '2');
+('2023-03-06 20:30:00', 6, 'M6', 1, '2'),
+('2023-03-07 21:30:00', 7, 'M7', 2, '6'),
+('2023-03-08 18:30:00', 8, 'M8', 3, '8'),
+('2023-03-09 19:00:00', 9, 'M9', 4, '4'),
+('2023-03-10 20:00:00', 10, 'M10', 5, '2');
 
 -- Inserir dados na tabela Fornecedores
 INSERT INTO Fornecedores (Nome, CNPJ, Tipo_material, Email, Telefone, Valor_material) VALUES
@@ -234,50 +231,60 @@ INSERT INTO Estoque (ID_fornecedor, Quantidade, ID_filial) VALUES
 (3, 150, 3),
 (4, 250, 4),
 (5, 300, 5),
-(6, 350, 6),
-(7, 400, 7),
-(8, 450, 8),
-(9, 500, 9),
-(10, 550, 10);
+(6, 350, 1),
+(7, 400, 2),
+(8, 450, 3),
+(9, 500, 4),
+(10, 550, 5);
 
 -- Inserir dados na tabela Pratos
 INSERT INTO Pratos (Nome, Descricao, Valor) VALUES
-('Prato 1', 'Descrição do Prato 1', 25.50),
-('Prato 2', 'Descrição do Prato 2', 30.00),
-('Prato 3', 'Descrição do Prato 3', 20.00),
-('Prato 4', 'Descrição do Prato 4', 15.00),
-('Prato 5', 'Descrição do Prato 5', 22.50),
-('Prato 6', 'Descrição do Prato 6', 18.00),
-('Prato 7', 'Descrição do Prato 7', 27.50),
-('Prato 8', 'Descrição do Prato 8', 32.00),
-('Prato 9', 'Descrição do Prato 9', 19.50),
-('Prato 10', 'Descrição do Prato 10', 21.00);
+('Bife à Parmegiana', 'Bife empanado com molho de tomate e queijo', 25.90),
+('Lasanha de Frango', 'Lasanha recheada com frango desfiado', 22.50),
+('Spaghetti Carbonara', 'Macarrão com molho à base de ovos e bacon', 20.00),
+('Salmão Grelhado', 'Filé de salmão grelhado com ervas', 30.90),
+('Frango à Milanesa', 'Filé de frango empanado e frito', 18.90),
+('Feijoada', 'Prato típico brasileiro com feijão preto e carnes', 35.00),
+('Arroz de Polvo', 'Arroz com pedaços de polvo e temperos', 28.50),
+('Risoto de Camarão', 'Risoto cremoso com camarão', 32.00),
+('Filé Mignon', 'Filé de carne bovina grelhado', 40.00),
+('Moqueca de Peixe', 'Peixe cozido com leite de coco e dendê', 27.50);
 
 -- Inserir dados na tabela Bebidas
 INSERT INTO Bebidas (Nome, Descricao, Valor) VALUES
-('Bebida 1', 'Descrição da Bebida 1', 5.50),
-('Bebida 2', 'Descrição da Bebida 2', 6.00),
-('Bebida 3', 'Descrição da Bebida 3', 4.50),
-('Bebida 4', 'Descrição da Bebida 4', 7.00),
-('Bebida 5', 'Descrição da Bebida 5', 5.00),
-('Bebida 6', 'Descrição da Bebida 6', 6.50),
-('Bebida 7', 'Descrição da Bebida 7', 4.00),
-('Bebida 8', 'Descrição da Bebida 8', 7.50),
-('Bebida 9', 'Descrição da Bebida 9', 5.00),
-('Bebida 10', 'Descrição da Bebida 10', 6.00);
+('Refrigerante', 'Bebida gaseificada', 5.00),
+('Suco de Laranja', 'Suco natural de laranja', 7.00),
+('Água Mineral', 'Água engarrafada sem gás', 3.00),
+('Cerveja', 'Bebida alcoólica fermentada', 8.00),
+('Vinho Tinto', 'Vinho produzido a partir de uvas tintas', 20.00),
+('Caipirinha', 'Coquetel de cachaça com limão', 15.00),
+('Whisky', 'Bebida alcoólica destilada de grãos', 25.00),
+('Café Expresso', 'Café feito sob pressão', 4.00),
+('Chá Gelado', 'Bebida refrescante de chá', 6.00),
+('Milkshake', 'Bebida gelada à base de sorvete', 10.00);
 
 -- Inserir dados na tabela Ingredientes
 INSERT INTO Ingredientes (Nome, Unidade_medida, ID_prato, ID_bebida) VALUES
-('Ingrediente 1', 'kg', 1, 1),
-('Ingrediente 2', 'g', 2, 2),
-('Ingrediente 3', 'ml', 3, 3),
-('Ingrediente 4', 'L', 4, 4),
-('Ingrediente 5', 'kg', 5, 5),
-('Ingrediente 6', 'g', 6, 6),
-('Ingrediente 7', 'ml', 7, 7),
-('Ingrediente 8', 'L', 8, 8),
-('Ingrediente 9', 'kg', 9, 9),
-('Ingrediente 10', 'g', 10, 10);
+('Tomate', 'Kg', 1, NULL),
+('Frango', 'Kg', 2, NULL),
+('Bacon', 'Kg', 3, NULL),
+('Salmão', 'Kg', 4, NULL),
+('Farinha de Rosca', 'Kg', 5, NULL),
+('Feijão Preto', 'Kg', 6, NULL),
+('Polvo', 'Kg', 7, NULL),
+('Camarão', 'Kg', 8, NULL),
+('Carne Bovina', 'Kg', 9, NULL),
+('Peixe', 'Kg', 10, NULL),
+('Gás Carbônico', 'L', NULL, 1),
+('Laranja', 'Unidade', NULL, 2),
+('Água', 'L', NULL, 3),
+('Malte', 'Kg', NULL, 4),
+('Uva', 'Kg', NULL, 5),
+('Cachaça', 'L', NULL, 6),
+('Cevada', 'Kg', NULL, 7),
+('Café', 'Kg', NULL, 8),
+('Chá', 'Kg', NULL, 9),
+('Sorvete', 'L', NULL, 10);
 
 -- Inserir dados na tabela Pedidos
 INSERT INTO Pedidos (ID_cliente, ID_prato, ID_bebida, Valor, Tipo_pagamento, Endereco, Data_pedido) VALUES
--- 1_Semestre/Avaliação_Final/Script.sql
+++ 1_Semestre/Avaliação_Final/Script.sql
@@ -1,7 +1,15 @@
 /*SELECT * 
 FROM Login 
-WHERE Username = 'mariasilva' AND Senha = 'senha123'; Script das informações de login*/
+WHERE Username = 'mariasilva' AND Senha = 'senha123'; /*Script das informações de login*/
 
 /*SELECT Nome
 FROM Funcionarios
-Where ID_filial = '1'; Script de funcionários de cada filial*/
+Where ID_filial = '1'; /*Script de funcionários de cada filial*/
+
+/*SELECT Nome, ID_filial
+FROM Clientes
+ORDER BY ID_filial ASC; /*Script de cada cliente organizado por filial*/
+
+/*SELECT *
+FROM Pratos
+Where Valor > 19.00; /*Script onde o gerente que escolher pratos acima de R$19 para ter desconto*/
