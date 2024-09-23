cd27b0c4d7d97f73d14a1c5aa0600c3a07ca00cc
Caduzin2723 <137653514+Caduzin2723@users.noreply.github.com>
2024-09-23 09:10:56 -0300

    Update Pokemon.sql

--- 2°_Semestre/Projeto Final/Pokemon.sql
+++ 2°_Semestre/Projeto Final/Pokemon.sql
@@ -1,25 +1,28 @@
 DROP DATABASE IF EXISTS Pokemon;
+
 CREATE DATABASE Pokemon;
+
 USE Pokemon;
 
 CREATE TABLE pokemon(
-	Nome varchar (50),
+    Nome varchar (50),
     Pokedex int (3),
     Nomedex varchar (50),
     Ataque1 varchar (50),
-	Ataque2 varchar (50),
-	Ataque3 varchar (50),
-	Ataque4 varchar (50),
+    Ataque2 varchar (50),
+    Ataque3 varchar (50),
+    Ataque4 varchar (50),
     Tipo1 varchar (50),
     Tipo2 varchar (50),
     Nature varchar (50),
     Pokebola varchar (50),
-    Item varchar (50),
+    Item_pokemon varchar (50),
     Combates int (5),
     Vitorias int (5),
     Derrotas int (5),
     Empate int (5),
-    Treinador varchar (50)
+    ID_Treinador varchar (50),
+    foreign key (ID_Treinador) references Treinadores(ID_Treinador)
     );
     
 CREATE TABLE Movimentos(
@@ -27,7 +30,7 @@ CREATE TABLE Movimentos(
 );
 
 CREATE TABLE Treinadores(
-	Nome varchar (50),
+    Nome varchar (50),
     ID_treinador varchar (50),
     Idade varchar (3),
     Pokemon1 varchar (50),
@@ -36,8 +39,13 @@ CREATE TABLE Treinadores(
     Pokemon4 varchar (50),
     Pokemon5 varchar (50),
     Pokemon6 varchar (50),
-    Insignias varchar (2),
-    
+    Insignias varchar (2)
 );
-    
-    
+
+CREATE TABLE Utilitarios(
+    ID_item varchar (50),
+    Nome varchar (50),
+    Descrição varchar (50),
+    Preço decimal (2,4),
+    Consumivel boolean
+);
